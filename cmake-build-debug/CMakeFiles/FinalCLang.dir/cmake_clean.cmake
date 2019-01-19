96e559a1fe307b7cf2466fae81226a2f40c34844
Ali <billa.svpc@gmail.com>
2019-01-19 16:30:39 +0330

    Main.c first commit

---
+++ cmake-build-debug/CMakeFiles/FinalCLang.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalCLang.dir/main.c.obj"
+  "FinalCLang.pdb"
+  "FinalCLang.exe"
+  "FinalCLang.exe.manifest"
+  "libFinalCLang.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FinalCLang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
